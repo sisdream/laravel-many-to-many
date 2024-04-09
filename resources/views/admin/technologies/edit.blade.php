5ae9f5447c37686159b2b8eada23a0dea0cf4f68
sisdream <sissipascale2@gmail.com>
2024-04-09 18:12:23 +0200

    fix: aggiusti sintassi

--- resources/views/admin/technologies/edit.blade.php
+++ resources/views/admin/technologies/edit.blade.php
@@ -6,20 +6,15 @@
     <div class="container">
         <a href="{{ route('admin.technologies.show', $technology) }}" class="btn btn-primary mt-4 mb-3">Torna al progetto</a>
         <a href="{{ route('admin.technologies.index') }}" class="btn btn-primary mt-4 mb-3">Torna alla lista</a>
-
         <h1 class="mb-3">Modifica {{ $technology->label }}</h1>
-
         <form action="{{ route('admin.technologies.update', $technology) }}" method="POST">
             @csrf
-
             @method('PATCH')
-
             <div class="row g-3">
                 <div class="col-6">
                     <label for="title" class="form-label">Etichetta</label>
                     <input technology="text" class="form-control @error('label') is-invalid @enderror" id="label"
                         name="label" value="{{ $errors->any() ? old('label') : $technology->label }}">
-
                     @error('label')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
