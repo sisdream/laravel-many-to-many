c8571e8f2088d6e6860ac9de0b43af8ae3242302
sisdream <sissipascale2@gmail.com>
2024-04-09 17:09:37 +0200

    fix: modifica modals

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -20,6 +20,10 @@ class Project extends Model
         return $this->belongsTo(Type::class);
     }
 
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class);
+    }
 
     public function getAbstract($n_chars = 30)
     {
--- app/Models/Type.php
+++ app/Models/Type.php
@@ -8,7 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Type extends Model
 {
     use HasFactory;
-
+    protected $fillable = [
+        'label',
+        'color',
+    ];
     public function projects()
     {
         return $this->hasMany(Project::class);
